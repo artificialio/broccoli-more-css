013d7402a37195f969c3e79f723fc67dd30f9802
Globegitter <markus.padourek@gmail.com>
2014-11-17 03:08:04 +0000

    Fix test description.

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ afterEach(function () {
   rimraf.sync('test/actual');
 });
 
-it('should minimize CSS without radical as expected', function () {
+it('should minimize CSS with radical as expected', function () {
   assert.equal(
     fs.readFileSync('test/actual/fixture.css', 'utf8'),
     fs.readFileSync('test/fixtures/expected.radical.css', 'utf8').trim()
